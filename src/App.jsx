06d38192972e17c50b86f303f5d6571549387a14
Shrinivas Kathewade <shripatil676@gmail.com>
2026-01-08 16:47:33 +0530

    Fix import path for Footer component
    fixing the import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import './Style.css'
 import { Outlet } from 'react-router-dom'
-import Footer from './Components/Footer.jsx'
+import Footer from './components/Footer.jsx'
 import appStore from './utils/MobileStore.js'
 import { Provider } from 'react-redux'
 
